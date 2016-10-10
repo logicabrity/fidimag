b5bb7c35b483f7b78c790886a12cbd58b70fae92
Ryan Pepper <ryan.pepper@soton.ac.uk>
2016-10-10 23:00:05 +0100

    Switch to memory view layout

--- fidimag/micro/lib/micro_clib.pyx
+++ fidimag/micro/lib/micro_clib.pyx
@@ -27,55 +27,55 @@ cdef extern from "micro_clib.h":
                            int nx, int ny, int nz, int *ngbs)
 
 
-def compute_exchange_field_micro(np.ndarray[double, ndim=1, mode="c"] m,
-                            np.ndarray[double, ndim=1, mode="c"] field,
-                            np.ndarray[double, ndim=1, mode="c"] energy,
-                            np.ndarray[double, ndim=1, mode="c"] Ms_inv,
+def compute_exchange_field_micro(double [:]  m,
+                            double [:] field,
+                            double [:] energy,
+                            double [:] Ms_inv,
                             A, dx, dy, dz, n,
-            		    np.ndarray[int, ndim=2, mode="c"] ngbs):
+            		    int [:, :] ngbs):
 
     compute_exch_field_micro(&m[0], &field[0], &energy[0], &Ms_inv[0], A,
                              dx, dy, dz, n, &ngbs[0, 0])
     
 
-def compute_dmi_field_bulk(np.ndarray[double, ndim=1, mode="c"] m,
-                            np.ndarray[double, ndim=1, mode="c"] field,
-                            np.ndarray[double, ndim=1, mode="c"] energy,
-                            np.ndarray[double, ndim=1, mode="c"] Ms_inv,
-                            np.ndarray[double, ndim=1, mode="c"] D,
-                            dx, dy, dz,
-                            n, np.ndarray[int, ndim=2, mode="c"] ngbs
+def compute_dmi_field_bulk(double [:] m,
+			   double [:] field,
+                           double [:] energy,
+                           double [:] Ms_inv,
+                           double [:] D,
+                           dx, dy, dz, n,
+                           int [:, :] ngbs
                             ):
 
     dmi_field_bulk(&m[0], &field[0], &energy[0], &Ms_inv[0], &D[0],
                    dx, dy, dz, n, &ngbs[0, 0])
     
-def compute_dmi_field_interfacial(np.ndarray[double, ndim=1, mode="c"] m,
-                            np.ndarray[double, ndim=1, mode="c"] field,
-                            np.ndarray[double, ndim=1, mode="c"] energy,
-                            np.ndarray[double, ndim=1, mode="c"] Ms_inv,
-                            np.ndarray[double, ndim=1, mode="c"] D,
+def compute_dmi_field_interfacial(double [:] m,
+                            double [:] field,
+                            double [:] energy,
+                            double [:] Ms_inv,
+                            double [:] D,
                             dx, dy, dz, 
-                            n, np.ndarray[int, ndim=2, mode="c"] ngbs
+                            n, int [:, :] ngbs
                             ):
 
     dmi_field_interfacial(&m[0], &field[0], &energy[0], &Ms_inv[0], &D[0],
                           dx, dy, dz, n, &ngbs[0, 0])
 
-def compute_anisotropy_micro(np.ndarray[double, ndim=1, mode="c"] m,
-                            np.ndarray[double, ndim=1, mode="c"] field,
-                            np.ndarray[double, ndim=1, mode="c"] energy,
-                            np.ndarray[double, ndim=1, mode="c"] Ms_inv,
-                            np.ndarray[double, ndim=1, mode="c"] Ku,
-                            np.ndarray[double, ndim=1, mode="c"] axis,
+def compute_anisotropy_micro(double [:] m,
+                            double [:] field,
+                            double [:] energy,
+                            double [:] Ms_inv,
+                            double [:] Ku,
+                            double [:] axis,
                             nx, ny, nz):
 
     compute_uniaxial_anis(&m[0], &field[0], &energy[0], &Ms_inv[0],
                           &Ku[0], &axis[0], nx, ny, nz)
 
-def compute_skyrmion_number(np.ndarray[double, ndim=1, mode="c"] m,
-                            np.ndarray[double, ndim=1, mode="c"] charge,
+def compute_skyrmion_number(double [:] m,
+                            double [:] charge,
                             nx, ny, nz,
-            		    np.ndarray[int, ndim=2, mode="c"] ngbs):
+            		    int [:, :] ngbs):
 
     return skyrmion_number(&m[0], &charge[0], nx, ny, nz, &ngbs[0, 0])
