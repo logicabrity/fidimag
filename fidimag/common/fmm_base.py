7600b135dd694d50747168109d38f6784e08c984
Ryan Pepper <ryan.pepper@soton.ac.uk>
2016-12-05 10:17:45 +0000

    Add prototypes for functions to start working on

--- fidimag/common/fmm_base.py
+++ fidimag/common/fmm_base.py
@@ -1,5 +1,26 @@
 from __future__ import division
 from __future__ import print_function
+import numpy as np
+import scipy
+
+def SphericalHarmonics(order):
+    """
+    Returns spherical harmonic function
+    """
+    pass
 
 
+def CartesianExpansion(order):
+    pass
+
+def ShiftFunction(x, y):
+    pass
+
+
+
+class Demag_FMM:
+    def __init__(self):
+        pass
+    def compute_field(self):
+        pass
 
