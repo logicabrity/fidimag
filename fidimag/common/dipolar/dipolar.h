aa39b71f0045108d7db59e7b1bf14b377d14b38f
Ryan Pepper <ryan.pepper@soton.ac.uk>
2017-01-25 11:16:33 +0000

    Add scalar potential functions to dipolar.h

--- fidimag/common/dipolar/dipolar.h
+++ fidimag/common/dipolar/dipolar.h
@@ -15,7 +15,27 @@ enum Type_Nij {
 };
 
 //==========================================
-//used for demag
+/* The following structs are not actually used by
+FFTW themselves - the name is slightly misleading.
+They basically hold several things that C functions
+for calculating the demagnetising energy need.
+
+(a)   i) System properties, e.g nx, ny, nz, scale
+     ii) Quantities derived from system properties,
+        e.g nxy, nxyz, lenx, leny, lenz
+
+(b)  Pointers to arrays for
+     * Demagnetising Tensor
+     * Arrays to hold spin * Ms
+     * Fourier transforms of these.
+(c)  FFTW plans.
+
+We may want to split up these things in future.
+
+Also, we could probably delete the demagnetising tensor
+after taking the fourier transform of it, as this is not
+needed after that.
+*/
 
 typedef struct {
 	int nx, ny, nz, nxy, nxyz;
@@ -58,20 +78,67 @@ typedef struct {
 
 } fft_demag_plan;
 
+typedef struct {
+	int nx, ny, nz, nxy, nxyz;
+	double dx, dy, dz;
+	int lenx, leny, lenz, lenxy, lenxyz;
+	int total_length;
+ 	double scale;
+	double *sxx;
+	double *syy;
+	double *szz;
+	double *sxy;
+	double *sxz;
+	double *syz;
+
+	fftw_complex *Sxx;
+	fftw_complex *Syy;
+	fftw_complex *Szz;
+	fftw_complex *Sxy;
+	fftw_complex *Sxz;
+	fftw_complex *Syz;
+
+	fftw_complex *Mx;
+	fftw_complex *My;
+	fftw_complex *Mz;
+	fftw_complex *Phi;
+
+	double *mx;
+	double *my;
+	double *mz;
+	double *phi;
+
+	//we need three plans
+	fftw_plan tensor_plan;
+	fftw_plan m_plan;
+	fftw_plan phi_plan;
+
+} fft_demag_plan_scalar;
+
 fft_demag_plan *create_plan(void);
 void finalize_plan(fft_demag_plan *plan);
-void init_plan(fft_demag_plan *plan, double dx, double dy,
-		double dz, int nx, int ny, int nz);
-void compute_dipolar_tensors(fft_demag_plan *plan); 
+void init_plan(fft_demag_plan *plan, double dx, double dy, double dz, int nx, int ny, int nz);
+void compute_dipolar_tensors(fft_demag_plan *plan);
 void compute_demag_tensors(fft_demag_plan *plan);
 void create_fftw_plan(fft_demag_plan *plan);
-
 void compute_demag_tensors_2dpbc(fft_demag_plan *plan, double *tensors, double pbc_2d_error, int sample_repeat_nx, int sample_repeat_ny, double dipolar_radius);
 void fill_demag_tensors_c(fft_demag_plan *plan, double *tensors);
-
 void compute_fields(fft_demag_plan *plan, double *spin, double *mu_s, double *field);
 void exact_compute(fft_demag_plan *plan, double *spin, double *mu_s, double *field);
 double compute_demag_energy(fft_demag_plan *plan, double *spin, double *mu_s, double *field);
+
+
+fft_demag_plan_scalar *create_plan_scalar(void);
+void finalize_plan_scalar(fft_demag_plan_scalar *plan);
+void init_plan_scalar(fft_demag_plan_scalar *plan, double dx, double dy, double dz, int nx, int ny, int nz);
+void compute_dipolar_tensors_scalar(fft_demag_plan_scalar *plan);
+void compute_demag_tensors_scalar(fft_demag_plan_scalar *plan);
+void create_fftw_plan_scalar(fft_demag_plan_scalar *plan);
+void compute_demag_tensors_2dpbc_scalar(fft_demag_plan_scalar *plan, double *tensors, double pbc_2d_error, int sample_repeat_nx, int sample_repeat_ny, double dipolar_radius);
+void fill_demag_tensors_c_scalar(fft_demag_plan_scalar *plan, double *tensors);
+void compute_fields_scalar(fft_demag_plan_scalar *plan, double *spin, double *mu_s, double *field);
+void exact_compute_scalar(fft_demag_plan_scalar *plan, double *spin, double *mu_s, double *field);
+double compute_demag_energy_scalar(fft_demag_plan_scalar *plan, double *spin, double *mu_s, double *field);
 
 
 
@@ -105,4 +172,3 @@ void run_step1(ode_solver *s, double *m, double *h, double *m_pred, double *T,
 		double *alpha, double *mu_s_inv, int *pins);
 void run_step2(ode_solver *s, double *m_pred, double *h, double *m, double *T,
 		double *alpha, double *mu_s_inv, int *pins);
-
